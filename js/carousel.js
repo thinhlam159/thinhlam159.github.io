30ac670f68600859e0fe4e9e81cd2285f50b8a15
thinhlam159 <lamvietthinh048@gmail.com>
2020-12-06 23:00:30 +0700

    first commit

---
+++ js/carousel.js
@@ -0,0 +1,236 @@
+const arrowLeft = document.querySelector(".slick-prev");
+const arrowRight = document.querySelector('.slick-next');
+const slickTrack = document.querySelector('.slick-track');
+const slickSlice = document.querySelectorAll('.slick-slide');
+const slickDots = document.querySelectorAll('.slick-dots li');
+const btn = document.querySelectorAll('.slick-dots button');
+
+let eleIsClicked = 0;
+let size = slickSlice[0].clientWidth;
+let count = 1, time = 2000;
+let stateTab = true;
+let stateTranslateOfSlickTrack = true;
+let v_interval = '';
+
+let hidden, visibilityChange;
+run_setInterval();
+
+//tao ham run setInterval va run clearInterval
+function run_setInterval() {
+    v_interval = setInterval(() => {
+        size = slickSlice[0].clientWidth;
+        slickDots[count-1].classList.remove('slick-active');
+        slickTrack.style.transition = 'transform .5s ease-in-out';
+        slickTrack.style.transform = `translate3d(${-size*(++count)}px,0px,0px)`;
+        eleIsClicked = count -1;
+        if(count === slickSlice.length -1 ) {
+            slickDots[0].classList.add('slick-active')
+        } else {
+            slickDots[count -1].classList.add('slick-active')
+        }
+        // console.log('interval '+ count)
+    }, time)
+}
+function run_clearInterval() {
+    clearInterval(v_interval)
+}
+
+//dung translate khi user chuyen qua tab khac
+if(typeof document.hidden !== "undefined") {
+    hidden = "hidden";
+    visibilityChange = "visibilitychange";
+} else if(typeof document.msHidden !== "undefined") {
+    hidden = "msHidden";
+    visibilityChange = "msvisibilitychange"
+} else if (typeof document.webkitHidden !== "undefined") {
+    hidden = "webkitHidden";
+    visibilityChange = "webkitvisibilitychange";
+}
+function handleVisibilityChange() {
+    stateTab = (document[hidden]) ? false : true;
+    if (stateTab) {
+        run_setInterval()
+    } else {
+        run_clearInterval()
+    }
+}
+
+//them su kien click cho arrow
+arrowLeft.addEventListener("click", function (e) {
+    if(stateTranslateOfSlickTrack) {
+        run_clearInterval();
+        commonFuncBothArrows(true, false, e);
+        run_setInterval();
+    }
+});
+arrowRight.addEventListener("click", function (e) {
+    if(stateTranslateOfSlickTrack) {
+        run_clearInterval();
+        commonFuncBothArrows(false, true, e);
+        run_setInterval();
+    }
+});
+function commonFuncBothArrows(arrowL, arrowR, e) {
+    e.preventDefault();
+    stateTranslateOfSlickTrack = false;
+    if(arrowL){
+        if(count <= 0 ){ return; }
+    }else{
+        if(arrowR){
+            if(count >= slickSlice.length - 1){ return;}
+        }
+    }
+    console.log(count + ' count');
+    slickDots[count - 1].classList.remove('slick-active');
+    slickTrack.style.transition = 'transform 0.5s ease-in-out';
+    count = arrowL ? --count : ++count;
+    slickTrack.style.transform = `translate3d(${-size*count}px, 0px, 0px)`;
+    eleIsClicked = count -1;
+    console.log(count + ' count');
+    switch (count) {
+        case 0:
+            slickDots[slickDots.length - 1].classList.add('slick-active');
+            break;
+        case slickSlice.length-1:
+            slickDots[0].classList.add('slick-active');
+            break;
+        default:
+            slickDots[count - 1].classList.add('slick-active');
+            break;
+    }
+
+}
+
+//the su kien click cho button
+btn.forEach(el => {
+    el.addEventListener("click", function () {
+        if (stateTranslateOfSlickTrack) {
+            run_clearInterval();
+            slickTrack.style.transition = "transform .5s ease-in-out";
+            count = Number(el.textContent);
+            slickDots[eleIsClicked].classList.remove("slick-active");
+            slickDots[count -1 ].classList.add("slick-active");
+            slickTrack.style.transform = `translate3d(${-size*count}px,0px,0px)`;
+            eleIsClicked = count - 1;
+            run_setInterval();
+        }
+    })
+});
+
+slickTrack.addEventListener('transitionend', ()=>{
+    stateTranslateOfSlickTrack = true; // cho biết thằng carousel nó đã thực hiện xong việc translateX
+    let nameClassSlickSlide = slickSlice[count].id;
+    if(nameClassSlickSlide === 'lastClone' || nameClassSlickSlide === 'firstClone'){
+        slickTrack.style.transition = `none`;
+        count = (nameClassSlickSlide === 'lastClone')?slickSlice.length - 2:(nameClassSlickSlide === 'firstClone')?1:count; // slickSlice .length - 2 thì 2 này là 1 cái ảnh ở đầu có id='lastClone' và 1 ảnh ở cuối có id = 'firstClone'
+        eleIsClicked = count - 1;
+        slickTrack.style.transform = `translateX(-${size*count}px)`;
+    }
+});
+//touch slide=======================================================
+const carousel = document.querySelector('.carousel');
+const carouselBox = document.querySelector('.carousel-items');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+// event.stopPropagation();
+touch_slide(carousel, carouselBox, prevButton, nextButton);
+
+function touch_slide(wrapper, items, prev, next) {
+    let posX1 = 0,
+        posX2 = 0,
+        posInitial,
+        posFinal,
+        threshold = 100,
+        slides = document.querySelectorAll('.carousel-item'),
+        slideLength = slides.length,
+        slideSize = document.querySelectorAll('.carousel-item')[0].offsetWidth,
+        index = 0,
+        allowShift = true;
+
+    // Mouse and touch events
+    items.onmousedown = dragStart;
+
+    // Touch Events
+
+    items.addEventListener('touchstart', dragStart);
+    items.addEventListener('touchmove', dragAction);
+    items.addEventListener('touchend', dragEnd);
+
+    // Click events
+    prev.addEventListener('click', function () {
+        shiftSlide(-1)
+    });
+    next.addEventListener('click', function () {
+        shiftSlide(1)
+    });
+
+    // Transition end
+    items.addEventListener('transitionend', checkIndex);
+
+    function dragStart(e) {
+        e = e || window.event;
+        // e.preventDefault();
+        if (allowShift === false) return;
+        posInitial = items.offsetLeft;
+
+        if (e.type === 'touchstart') {
+            posX1 = e.touches[0].clientX;
+        } else {
+            posX1 = e.clientX;
+            document.onmouseup = dragEnd;
+            document.onmousemove = dragAction;
+            // console.log(posX1)
+        }
+    }
+
+    function dragAction(e) {
+        e = e || window.event;
+        if (allowShift === false) return;
+        if(e.type === 'touchmove') {
+            posX2 = e.touches[0].clientX - posX1;
+            posX1 = e.touches[0].clientX
+        } else {
+            posX2 = e.clientX - posX1;
+            posX1 = e.clientX;
+            // console.log(posX2)
+        }
+        items.style.left = (items.offsetLeft + posX2) + 'px';
+        // console.log(items)
+    }
+    function dragEnd() {
+        posFinal = items.offsetLeft;
+        if (posFinal - posInitial > threshold) {
+            shiftSlide(-1, 'drag')
+        } else if (posFinal - posInitial < -threshold) {
+            shiftSlide(1, 'drag')
+        } else {
+            items.style.left = posInitial + 'px';
+        }
+
+        document.onmousemove = null;
+        document.onmouseup = null;
+    }
+    function shiftSlide(dir, action) {
+        slideSize = document.querySelectorAll('.carousel-item')[0].offsetWidth;
+        items.classList.add('shifting');
+        if(!action) {
+            posInitial = items.offsetLeft
+        }
+        if(dir === 1) {
+            items.style.left = -(slideSize*4 -2) + 'px';
+        } else if(dir === -1) {
+            items.style.left = '0px';
+        }
+        allowShift = false
+    }
+    function checkIndex(event) {
+        event.target.classList.remove('shifting');
+        allowShift = true;
+    }
+}
+if (typeof(Storage) !== "undefined") {
+    var domain = 'freetuts.net';
+    localStorage.setItem('domain', domain);
+} else {
+    document.write('Trình duyệt của bạn không hỗ trợ local storage');
+}
---
+++ js/script.js
@@ -0,0 +1,118 @@
+//===============add to cart======================
+const cart__removeItem = document.querySelectorAll('.cart-item__remove-item-btn');
+const emptyCartDiv = document.querySelector('.empty');
+const cartItemButtonDiv = document.querySelector('.cart-item__total');
+const cartItems = document.querySelector('.cart-items');
+updateCart();
+
+for (let i = 0; i < cart__removeItem.length; i++) {
+    let btn = cart__removeItem[i];
+    btn.addEventListener('click', function (e) {
+        let removeBtn = event.target;
+        removeBtn.parentElement.parentElement.remove();
+        updateCart();
+    })
+}
+
+function updateCart() {
+    const cartItem = cartItems.querySelectorAll('.cart-item');
+    let itemCount = 0;
+    let total = 0;
+    for (let i = 0; i < cartItem.length; i++) {
+        let cart_Item = cartItem[i];
+        let cartPrice = cart_Item.querySelector('.cart-item__price');
+        let cartCount = cart_Item.querySelector('.cart-item__count');
+        let price = parseInt(cartPrice.textContent.substr(1));
+        let count = parseInt(cartCount.textContent.substr(1));
+        total += price * count;
+        itemCount +=count;
+    }
+    console.log(total);
+    document.querySelector('.cart-item__sub-total strong').textContent = '$' + total;
+    document.getElementById('total_count').textContent = `${itemCount}`;
+    // console.log(cartItem)
+    if (total !== 0) {
+        emptyCartDiv.style.display = 'none';
+        cartItemButtonDiv.style.display = 'flex';
+    } else {
+        cartItemButtonDiv.style.display = 'none';
+        emptyCartDiv.style.display = 'block';
+    }
+}
+
+//them gio hang
+const addToCartButton = document.querySelectorAll('.add-to-cart-btn');
+addToCartButton.forEach(element => {
+    element.addEventListener('click', function (event) {
+        let button = event.currentTarget;
+        let product = button.parentElement.parentElement.parentElement;
+        let src = product.querySelector('img').src;
+        let productName = product.querySelector('.product-name').textContent;
+        let price = product.querySelector('.price').textContent;
+        addToCart(src, productName, price);
+        updateCart();
+        console.log(src+productName+price)
+    })
+});
+
+function addToCart(img, name, price) {
+    console.log(name);
+    let avaiItem = false;
+    let cartTitles = cartItems.querySelectorAll('.title');
+    cartTitles.forEach(element => {
+        if (element.textContent === name) {
+            avaiItem = true;
+            let oldCount = element.parentElement.querySelector('.cart-item__count');
+            let numb = parseInt(oldCount.textContent.substr(1));
+            numb +=1;
+            let newCount = document.createElement('div');
+            newCount.classList.add('cart-item__count');
+            newCount.textContent = 'x' + numb;
+            element.parentNode.replaceChild(newCount, oldCount);
+        }
+    });
+    if(avaiItem === true) return;
+    let div = document.createElement('div');
+    let cartItemTotal = document.querySelector('.cart-item__total');
+    div.classList.add('cart-item');
+    // let price1 = '$' + price;
+    let newItem = `<div class="cart-item__row">
+                   <img src="${img}" alt="a">
+                   <div class="title">${name}</div>
+                   <div class="cart-item__count">x1</p></div>
+                   <div class="cart-item__price">${price}</div>
+                   <span class="cart-item__remove-item-btn">x</span>
+                   </div>
+                   `;
+    div.innerHTML = newItem;
+    cartItems.insertBefore(div, cartItemTotal);
+    div.querySelector('.cart-item__remove-item-btn').addEventListener('click', function (event) {
+        let removeItem = event.target;
+        removeItem.parentElement.parentElement.remove();
+        updateCart();
+    });
+}
+
+// =================modal====================
+const quickViewBtn = document.querySelectorAll('.quick-view-btn');
+const modalSection = document.querySelector('.section-modal');
+const closeModalBtn = modalSection.querySelector('.modal__close-btn');
+const modalBg = modalSection.querySelector('.modal');
+quickViewBtn.forEach(element => {
+    element.addEventListener('click', function () {
+        modalSection.className += ' active';
+    })
+});
+closeModalBtn.onclick = function () {
+    modalSection.classList.remove('active');
+};
+// modalSection.addEventListener('click', function (event) {
+//     event.target.classList.remove('active');
+// });
+modalBg.addEventListener('click', function (e) {
+    // e = e || window.event;
+    // modalSection.classList.remove('active');
+    let el = e.target.parentNode;
+    el.classList.remove('active');
+    // console.log(el)
+});
